f5c89221e4165d0da87de2595734ce2eb035ba92
Sergio Bonaque Rubio <s.bonaquecontacto@gmail.com>
2025-11-06 22:08:55 +0000

    proyecto final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -12,22 +12,22 @@ const Home = () => {
 		{
 			title: "Card 1",
 			description: "This is the first card.",
-			image: rigoImage,
+			image: "https://i.pinimg.com/474x/69/31/28/69312834b43954f7bf85b7e8c3d43a86.jpg",
 		},
 		{
 			title: "Card 2",
 			description: "This is the second card.",
-			image: rigoImage,
+			image: "https://i.pinimg.com/originals/f7/a5/f0/f7a5f0a7acb42c0efbfb08643abebccb.jpg",
 		},
 		{
 			title: "Card 3",
 			description: "This is the third card.",
-			image: rigoImage,
+			image: "https://preview.redd.it/show-me-your-most-ferocious-cat-pic-v0-qfxht2ona4gd1.jpeg?width=1280&auto=webp&s=2f4ae8f13179d32ebd8139e192c928409a6a55af",
 		},
 		{
 			title: "Card 4",
 			description: "This is the fourth card.",
-			image: rigoImage,
+			image: "https://preview.redd.it/whats-the-scariest-picture-you-have-of-your-cat-v0-slya394an9jd1.jpeg?width=637&auto=webp&s=b059eebcdce58bf65f1ceaf26821a0845421fae3",
 		}
 
 
