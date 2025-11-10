05e9a21087211900f6b1c23cbde5942d2c19753b
Sergio Bonaque Rubio <s.bonaquecontacto@gmail.com>
2025-11-10 12:22:59 +0000

    modificacion

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,17 +1,32 @@
-export const Navbar = () => (
-    <nav className="navbar navbar-expand-lg bg-dark-subtle ">
-        <div className=" container">
-            <a className="navbar-brand d-flex" href="#">Navbar</a>
-            <div className="container">
-            </div>
-            <div className="collapse navbar-collapse  " id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    <a className="nav-link" href="#">About</a>
-                    <a className="nav-link" href="#">Services</a>
-                    <a className="nav-link disabled" aria-disabled="true">Contact</a>
+export const Navbar = () => {
+    return (
+
+        <>
+          
+            {/* Navbar que colapsa  */}
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Start Boostrap</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            
+                                
+
+                           
+                        </ul>
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link" href="#">About</a>
+                            <a className="nav-link" href="#">Services</a>
+                            <a className="nav-link disabled" aria-disabled="true">Contact</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </nav>
-)
+            </nav>
+        </>
+    )
+}
+
